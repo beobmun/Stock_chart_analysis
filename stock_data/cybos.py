a992fcbdf08cb471da08479639b813adb2845041
unknown <behan@pusan.ac.kr>
2025-07-08 13:33:31 +0900

    Feat: can get KOSPI 5min stock data

--- stock_data/cybos.py
+++ stock_data/cybos.py
@@ -1,6 +1,7 @@
 import win32com.client
 from enum import IntEnum
 import pandas as pd
+from tqdm import tqdm
 
 class CPE_MARKET_KIND(IntEnum):
     NULL = 0
@@ -45,16 +46,16 @@ class CpSysDib:
     def __init__(self):
         self.StockChart = win32com.client.Dispatch("CpSysDib.StockChart")
         
-    def get_stock_data(self, code):
+    def get_stock_data(self, code, start, end):
         self.StockChart.SetInputValue(0, code) # 종목 선택
         self.StockChart.SetInputValue(1, ord('1')) # '1': 기간으로 요청, '2': 개수로 요청
-        self.StockChart.SetInputValue(2, 20241231) # 요청 종료일
-        self.StockChart.SetInputValue(3, 20150101) # 요청 시작일
+        self.StockChart.SetInputValue(2, end) # 요청 종료일
+        self.StockChart.SetInputValue(3, start) # 요청 시작일
         self.StockChart.SetInputValue(5, (0, 1, 2, 3, 4, 5, 8)) # 0:날짜, 1:시간, 2:시가, 3:고가, 4:저가, 5:종가, 8:거래량
         self.StockChart.SetInputValue(6, ord('m')) # 차트 구분: 'm': 분
         self.StockChart.SetInputValue(7, 5) # 주가
         self.StockChart.SetInputValue(9, ord('1')) # '0': 무수정 주가, '1':수정주가
-        #self.StockChart.SetInputValue(10, ord('3'))
+        self.StockChart.SetInputValue(10, ord('3'))
         
         self.StockChart.BlockRequest()
         
@@ -69,4 +70,32 @@ class CpSysDib:
                 temp.append(self.StockChart.GetDataValue(i, j))
             df[name_field[i]] = temp
         
-        return df
+        return df.loc[::-1].reset_index(drop=True)
+    
+    def _make_period(self, start, end):
+        s_y = int(start[:4])
+        s_m = int(start[4:])
+        e_y = int(end[:4])
+        e_m = int(end[4:])
+        period = list()
+        while s_y < e_y or s_m <= e_m:
+            if s_m < 10:
+                sm = '0'+str(s_m)
+            else:
+                sm = str(s_m)
+            s = str(s_y) + sm + '01'
+            e = str(s_y) + sm + '31'
+            period.append((s, e))
+            s_m += 1
+            if s_m > 12:
+                s_m = 1
+                s_y += 1
+        return period
+            
+    def get_stock_data_p(self, code, start, end):
+        p = self._make_period(start, end)
+        df = list()
+        with tqdm(p, desc=f"get data {code}", leave=False) as pbar:
+            for s, e in pbar:
+                df.append(self.get_stock_data(code, s, e))
+        return pd.concat(df).reset_index(drop=True)
--- stock_data/get_stock_data.py
+++ stock_data/get_stock_data.py
@@ -1,6 +1,8 @@
 import win32com.client
 import pandas as pd
 from cybos import CpUtil, CpSysDib, CPE_MARKET_KIND
+from tqdm import tqdm
+import os
 
 
 print("Cybos 연결 준비")
@@ -13,6 +15,9 @@ print("KOSPI 종목:")
 print(kospi_list[:5])
 
 cpSysDib = CpSysDib()
-df = cpSysDib.get_stock_data(kospi_list[1])
-print('---')
-print(df)
+
+# Kospi 5분봉 데이터 불러오기 & csv 파일 저장
+os.makedirs("./data/KOSPI",exist_ok=True)
+for code in tqdm(kospi_list[:3], desc="Get KOSPI", position=0):
+    tmp_df = cpSysDib.get_stock_data_p(code, '202006', '202506')
+    tmp_df.to_csv(f"./data/KOSPI/{code}.csv", sep=',', index=False)
